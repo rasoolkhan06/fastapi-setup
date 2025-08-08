67502d5fa6e7344923ea7cc965c007e41812042d
rasoolkhan06 <rs.khan1997@gmail.com>
2025-08-08 16:17:34 +0530

    add config file

---
+++ app/core/config.py
@@ -0,0 +1,80 @@
+import os
+from typing import List
+
+from dotenv import load_dotenv
+from pydantic import BaseSettings
+
+load_dotenv()
+
+ENV: str = ""
+
+class Configs(BaseSettings):
+    # base
+    ENV: str = os.getenv("ENV", "dev")
+    API: str = "/api"
+    API_V1_STR: str = "/api/v1"
+    PROJECT_NAME: str = "fast-api"
+    ENV_DATABASE_MAPPER: dict = {
+        "prod": "fast-api",
+        "stage": "stage-fast-api",
+        "dev": "dev-fast-api",
+        "test": "test-fast-api",
+    }
+    DB_ENGINE_MAPPER: dict = {
+        "postgresql": "postgresql",
+        "mysql": "mysql+pymysql",
+    }
+
+    PROJECT_ROOT: str = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+    # date
+    DATETIME_FORMAT: str = "%Y-%m-%dT%H:%M:%S"
+    DATE_FORMAT: str = "%Y-%m-%d"
+
+    # auth
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 30  # 60 minutes * 24 hours * 30 days = 30 days
+
+    # CORS
+    BACKEND_CORS_ORIGINS: List[str] = ["*"]
+
+    # database
+    DB: str = "mysql"
+    DB_USER: str = "root"
+    DB_PASSWORD: str = ""
+    DB_HOST: str = "localhost"
+    DB_PORT: str = "3306"
+    DB_ENGINE: str = DB_ENGINE_MAPPER[DB]
+
+    DATABASE_URI_FORMAT: str = "{db_engine}://{user}:{password}@{host}:{port}/{database}"
+
+    DATABASE_URI = DATABASE_URI_FORMAT.format(
+        db_engine=DB_ENGINE,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT,
+        database=ENV_DATABASE_MAPPER[ENV],
+    )
+
+    # find query
+    PAGE = 1
+    PAGE_SIZE = 20
+    ORDERING = "-id"
+
+    class Config:
+        case_sensitive = True
+
+
+class TestConfigs(Configs):
+    ENV: str = "test"
+
+
+configs = Configs()
+
+if ENV == "prod":
+    pass
+elif ENV == "stage":
+    pass
+elif ENV == "test":
+    setting = TestConfigs()
